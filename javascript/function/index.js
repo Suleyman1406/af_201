2ee30646a530ecb3d1472598e57b8c4910e735ce
suleyman1406 <dadasovsuleyman126@gmail.com>
2022-11-28 21:01:15 +0400

    js functions added

---
+++ javascript/function/index.js
@@ -0,0 +1,92 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function printHello(name1, name2, name3,name4){
+//   // console.log('Hello '+name1+', '+name2+', '+name3+name4)
+//   // console.log(`Hello ${name1}, ${name2}, ${name3}, ${name4}`)
+//   return `Hello ${name1}, ${name2}, ${name3}, ${name4}`
+// }
+
+// printHello('Niyaz', 'Ebulfaz', "vahid", "Vasif");
+
+// let herhansiBirDeyisken = printHello('Niyaz', 'Ebulfaz', "vahid", "Vasif");
+
+// console.log(printHello('Niyaz', 'Ebulfaz', "vahid", "Vasif")); 
+
+// function sum(a,b){
+//   return (a+b)/2;
+// }
+
+// sum(5,4);
+// console.log(sum)
+// console.log(sum(4,5))
+
+// var sum = function(a,b){
+//   return a+b;
+// }
+
+
+// function sum(a,b){
+//   return a+b
+// }
+
+
+// let sum = a =>{
+//   return a*2;
+// }
+
+// console.log(sum( 5,4));
+
+// function sum(a,b){
+//   console.log('1. calisdi');
+//   console.log(a+b);
+// }
+
+// function sum(a,b,c){
+//   console.log('2. calisdi');
+//   console.log(a+b+c);
+// }
+
+// sum(1,2)
+
+
+// function sumAll(a,b,c,functionP){
+//   console.log(functionP)
+//   let sum = a+b+c;
+//   // functionP(sum)
+// }
+
+// let pow = (a)=>{
+//   console.log(a*a)
+// }
+
+// sumAll(1,2,3,pow);
+
+
+let sum = (a,b)=>{
+  return a+b;
+}
+
+console.log(sum(1,2))
